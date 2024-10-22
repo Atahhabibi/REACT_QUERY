6ff373f37843bc8d58121227ea3e8303bcebec0e
atahhabibi <habibiatah@gmail.com>
2024-10-22 13:17:49 -0700

    initial

---
+++ starter/src/Items.jsx
@@ -0,0 +1,28 @@
+import SingleItem from './SingleItem';
+import { useQuery } from "@tanstack/react-query";
+import customFetch from './util';
+
+
+
+
+
+
+
+const Items = ({ items }) => {
+
+  const result = useQuery({
+    queryKey:['tasks'],
+    queryFn:()=>customFetch.get('/')
+  });
+
+  console.log(result);
+
+  return (
+    <div className='items'>
+      {items.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
